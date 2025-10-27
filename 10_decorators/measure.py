1bfd70ee315824aa175e35fa75f272e5f47a2b6d
Dash Barrett <dbarrett7614@floridapoly.edu>
2025-10-27 15:13:49 +0000

    Learning about Decorators and what they can do

---
+++ 10_decorators/measure.py
@@ -0,0 +1,15 @@
+from time import sleep, time
+
+def measure(func, *args, **kwargs): # a decorator
+    def wrapper(*args, **kwargs):
+        t = time() # get current time
+        func(*args, **kwargs) # call the received function
+        print(func.__name__, "took:", time() - t)
+    return wrapper
+    
+@measure
+def f2(sleep_time = 0.1):
+    sleep(sleep_time)
+
+if __name__ == '__main__': 
+    print("running directly")
